3309c567841fa5d57659adf71b74f25a5ed78fc6
Joseph Kandi <joseph@apps4u.co.za>
2016-08-05 15:05:39 +0200

    Removed weird charcters

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 var express = require("express");
 var app = express();
-var fib = function (n) {
- if (n === 0) {
- return 0;
- } else if (n == 1) {
- return 1;
- } else {
- return fib(n — 1) + fib(n — 2)
- }
+
+var PORT = process.env.PORT || 3000;
+
+var fib = function(n) {
+    if (n === 0){
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
 };
+
 app.get("/:number", function (req, res) {
- var number = req.param("number"); 
- var result = fib(number);
- res.send("Node + Express<hr> fib("+number+"): "+result); 
+    var number = req.param("number"); 
+    var result = fib(number);
+    res.send("Node + Express<hr> fib("+number+"): "+result); 
 });
-app.listen(3000);
+
+console.log('Server running on port ' + PORT);
+app.listen(PORT);
