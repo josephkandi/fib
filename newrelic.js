db3c3eaa8035ae39db1a6f67ecf7b6e255de8794
Joseph Kandi <joseph@apps4u.co.za>
2016-08-08 10:45:01 +0000

    Added newrelic monitoring agent

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['FIB API'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '772f75e14a6881fd8d7406fa616a1a14a109ef7b',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
